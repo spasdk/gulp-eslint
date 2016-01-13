831e1ba83acdc2aad0ab6f2249ba1adb7c2807d6
Stanislav Kalashnik <sk@infomir.eu>
2016-01-13 17:48:45 +0200

    remove "profiles" level from config

--- config.js
+++ config.js
@@ -9,33 +9,30 @@
 
 var path = require('path');
 
-// public
+// set of named configs for corresponding gulp tasks
+// each profile inherits all options from the "default" profile
 module.exports = {
-    // set of named configs for corresponding gulp tasks
-    // each profile inherits all options from the "default" profile
-    profiles: {
-        // config to be extended by other profiles
-        default: {
-            // apply some visualization to the lint results
-            // available formatters - https://github.com/eslint/eslint/tree/master/lib/formatters
-            format: 'stylish',
+    // config to be extended by other profiles
+    default: {
+        // apply some visualization to the lint results
+        // available formatters - https://github.com/eslint/eslint/tree/master/lib/formatters
+        format: 'stylish',
 
-            //
-            popup: true
-        },
+        //
+        popup: true
+    },
 
-        // config for lint:check:develop task
-        develop: {
-            // glob or an array of globs of files to process
-            // see format in https://github.com/isaacs/node-glob
-            sourceMask: [
-                path.join(process.env.PATH_SRC, 'js', '**', '*.js'),
-                path.join(process.env.PATH_CFG, '**', '*.js')
-            ],
+    // config for lint:check:develop task
+    develop: {
+        // glob or an array of globs of files to process
+        // see format in https://github.com/isaacs/node-glob
+        sourceMask: [
+            path.join(process.env.PATH_SRC, 'js', '**', '*.js'),
+            path.join(process.env.PATH_CFG, '**', '*.js')
+        ],
 
-            // create task lint:watch:develop
-            // to automatically rescan on source files change
-            watch: false
-        }
+        // create task lint:watch:develop
+        // to automatically rescan on source files change
+        watch: false
     }
 };

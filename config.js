efa19b192d13a7ded68464ba0ec7523dd60cf6df
Stanislav Kalashnik <sk@infomir.eu>
2016-01-13 18:45:41 +0200

    rework config

--- config.js
+++ config.js
@@ -12,18 +12,7 @@ var path = require('path');
 // set of named configs for corresponding gulp tasks
 // each profile inherits all options from the "default" profile
 module.exports = {
-    // config to be extended by other profiles
     default: {
-        // apply some visualization to the lint results
-        // available formatters - https://github.com/eslint/eslint/tree/master/lib/formatters
-        format: 'stylish',
-
-        //
-        popup: true
-    },
-
-    // config for lint:check:develop task
-    develop: {
         // glob or an array of globs of files to process
         // see format in https://github.com/isaacs/node-glob
         sourceMask: [
@@ -31,8 +20,12 @@ module.exports = {
             path.join(process.env.PATH_CFG, '**', '*.js')
         ],
 
-        // create task lint:watch:develop
-        // to automatically rescan on source files change
+        // apply some visualization to the lint results
+        // available formatters - https://github.com/eslint/eslint/tree/master/lib/formatters
+        format: 'stylish',
+
+        // create watch task
+        // to automatically rebuild on source files change
         watch: false
     }
 };

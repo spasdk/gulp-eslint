a657e1270ef1d31f2269c62672046ee229331fa7
Stanislav Kalashnik <sk@infomir.eu>
2016-01-26 19:54:04 +0200

    rework config handling

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 /**
- * Configuration for lint gulp task.
+ * Base configuration for lint gulp task.
  *
  * @author Stanislav Kalashnik <darkpark.main@gmail.com>
  * @license GNU GENERAL PUBLIC LICENSE Version 3
@@ -7,44 +7,36 @@
 
 'use strict';
 
-var path   = require('path'),
-    extend = require('extend'),
-    config = require('spa-gulp/config');
+var path     = require('path'),
+    extend   = require('extend'),
+    config   = require('spa-gulp/config'),
+    source   = [path.join(config.source, 'js', '**', '*.js')],
+    profiles = {};
 
 
-// base config
-// each profile inherits all options from the "default" profile
-module.exports = extend(true, {}, config, {
-    default: {
-        // array of file globs to process
-        // see format in https://github.com/isaacs/node-glob
-        source: [
-            path.join(config.default.source, 'js', '**', '*.js')
-        ],
+// main
+profiles.default = extend(true, {}, config, {
+    // file globs to process
+    source: source,
 
-        // apply some visualization to the lint results
-        // available formatters - https://github.com/eslint/eslint/tree/master/lib/formatters
-        format: 'stylish',
+    // apply some visualization to the lint results
+    // available formatters - https://github.com/eslint/eslint/tree/master/lib/formatters
+    format: 'stylish',
 
-        // info channels
-        notifications: {
-            popup: {
-                info: {
-                    icon: path.join(__dirname, 'media', 'info.png')
-                },
-                warn: {
-                    icon: path.join(__dirname, 'media', 'warn.png')
-                },
-                fail: {
-                    icon: path.join(__dirname, 'media', 'fail.png')
-                }
-            }
-        },
+    // info channels
+    notifications: {
+        popup: {
+            info: {icon: path.join(__dirname, 'media', 'info.png')},
+            warn: {icon: path.join(__dirname, 'media', 'warn.png')},
+            fail: {icon: path.join(__dirname, 'media', 'fail.png')}
+        }
+    },
 
-        // false to prevent watch task creation
-        // otherwise array of globs to monitor
-        watch: [
-            path.join(config.default.source, 'js', '**', '*.js')
-        ]
-    }
+    // false to prevent watch task creation
+    // otherwise array of globs to monitor
+    watch: source
 });
+
+
+// public
+module.exports = profiles;

a23a53aa96c7d28dbe2c30afb4400cb8530c1ab7
DarkPark <darkpark@pisem.net>
2016-02-20 23:49:08 +0200

    rework watch

--- index.js
+++ index.js
@@ -16,41 +16,43 @@ var util   = require('util'),
 // create tasks for profiles
 plugin.profiles.forEach(function ( profile ) {
     // lint + watch
-    profile.watch(profile.task(plugin.entry, function () {
-        var files = [];
+    profile.watch('', profile.data.watch,
+        profile.task(plugin.entry, function () {
+            var files = [];
 
-        return gulp
-            .src(profile.data.source || [])
-            .pipe(cache('eslint'))
-            .pipe(eslint())
-            .pipe(eslint.results(function ( results ) {
-                // collect all problematic files
-                results.forEach(function ( result ) {
-                    if ( result.errorCount || result.warningCount ) {
-                        files.push(util.format('%s %s/%s',
-                            result.filePath, result.errorCount, result.warningCount
-                        ));
-                    }
-                });
-
-                // success message
-                if ( files.length === 0 ) {
-                    profile.notify({
-                        title: plugin.entry,
-                        message: 'no problems found'
+            return gulp
+                .src(profile.data.source || [])
+                .pipe(cache('eslint'))
+                .pipe(eslint())
+                .pipe(eslint.results(function ( results ) {
+                    // collect all problematic files
+                    results.forEach(function ( result ) {
+                        if ( result.errorCount || result.warningCount ) {
+                            files.push(util.format('%s %s/%s',
+                                result.filePath, result.errorCount, result.warningCount
+                            ));
+                        }
                     });
-                }
-            }))
-            .pipe(eslint.format(profile.data.format, function ( result ) {
-                // failure message
-                profile.notify({
-                    type: 'fail',
-                    info: result,
-                    title: plugin.entry,
-                    message: files
-                });
-            }));
-    }));
+
+                    // success message
+                    if ( files.length === 0 ) {
+                        profile.notify({
+                            title: plugin.entry,
+                            message: 'no problems found'
+                        });
+                    }
+                }))
+                .pipe(eslint.format(profile.data.format, function ( result ) {
+                    // failure message
+                    profile.notify({
+                        type: 'fail',
+                        info: result,
+                        title: plugin.entry,
+                        message: files
+                    });
+                }));
+        })
+    );
 });
 
 
